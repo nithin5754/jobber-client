05fa46ab0dffd4011d55584fd269240bea9800f4
Nithin Joji0756 <nithinjoji0756@gmail.com>
2025-02-05 19:48:57 +0530

    updating

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-white/50">© 2025 <a href="/" className="hover:underline">codehirepro™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-white/50">© 2025. <a href="/" className="hover:underline">codehirepro™</a>. All Rights Reserved.</span>
     </div>
 </footer>
 
